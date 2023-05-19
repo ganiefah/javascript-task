adf48b00c67e0edd3340f33eced904c234be081d
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-19 09:59:00 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,31 @@
+// Challenge
+let first_number = 15;
+let second_number = 17 ;
+//first way 
+// var first_number = 15;
+// var second_number = 17; 
+
+// first_number = 17;
+// second_number = 15;
+
+// console.log(first_number);
+// console.log(second_number);
+
+//second way 
+// [first_number,second_number ] = [second_number, first_number]
+// console.log(first_number);
+// console.log(second_number);
+
+
+//third way 
+//  if (first_number = 15) {
+//     console.log(second_number);
+//  }else{
+//    console.log(17);
+//  }
+
+//  if (second_number= 17) {
+//    console.log(first_number);
+//  }else{
+//    console.log(15);
+//  }
